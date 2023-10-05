97d4bc68bf3e617ccdcd0ebe93ae5dd5a13e8332
AdamTa <adamtassaft05@gmail.com>
2023-10-05 15:37:01 -0700

    5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -3,10 +3,12 @@
 /**
  * main - Entry point
  *
+ * Description: Prints the desired output with proper grammar.
+ *
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-    printf("with proper grammar, but the outcome is a piece of art,\n");
-    return (0);
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
 }
