00a826cc22c328e493933f779a074430a691b9f0
AdamTa <adamtassaft05@gmail.com>
2023-10-06 08:36:36 -0700

    9

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,39 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int digit1 = 0;
+
+while (digit1 <= 9)
+{
+int digit2 = digit1 + 1;
+
+while (digit2 <= 9)
+{
+putchar(digit1 + '0');
+putchar(',');
+putchar(' ');
+putchar(digit2 + '0');
+
+if (digit1 != 8 || digit2 != 9)
+{
+putchar(',');
+putchar(' ');
+}
+
+digit2++;
+}
+
+digit1++;
+}
+
+putchar('\n');
+
+return (0);
+}
+
