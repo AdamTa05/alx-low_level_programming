63b9ec5e8e6cae08d101875bd9766cc7b1385b11
AdamTa <adamtassaft05@gmail.com>
2023-10-06 08:42:19 -0700

    9

--- 0x01-variables_if_else_while/9-print_comb.c
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -5,24 +5,25 @@
  *
  * Return: Always 0 (Success)
  */
+
 int main(void)
 {
-    int digit = 0;
+int digit = 0;
 
-    while (digit <= 9)
-    {
-        putchar(digit + '0');
+while (digit <= 9)
+{
+putchar(digit + '0');
 
-        if (digit != 9)
-        {
-            putchar(',');
-            putchar(' ');
-        }
+if (digit != 9)
+{
+putchar(',');
+putchar(' ');
+}
 
-        digit++;
-    }
+digit++;
+}
 
-    putchar('\n');
+putchar('\n');
 
-    return (0);
+return (0);
 }
