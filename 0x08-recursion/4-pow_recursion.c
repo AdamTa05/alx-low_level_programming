744962ee1df1de72d9c3c23f90c9be71a6025c21
AdamTa <adamtassaft05@gmail.com>
2023-10-25 13:38:56 -0700

    4

--- 0x08-recursion/4-pow_recursion.c
+++ 0x08-recursion/4-pow_recursion.c
@@ -9,8 +9,8 @@
 int _pow_recursion(int x, int y)
 {
 if (y < 0)
-return -1;  /* If y is negative, return -1 to indicate an error. */
+return (-1);  /* If y is negative, return -1 to indicate an error. */
 if (y == 0)
-return 1;   /* Base case: x^0 is 1. */
-return x * _pow_recursion(x, y - 1);  /* Recursive case. */
+return (1);   /* Base case: x^0 is 1. */
+return (x * _pow_recursion(x, y - 1));  /* Recursive case. */
 }
