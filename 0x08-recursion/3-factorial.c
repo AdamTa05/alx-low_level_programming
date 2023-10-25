79cf686d9cd76923e9a30af43c1787cc6f61c98a
AdamTa <adamtassaft05@gmail.com>
2023-10-25 13:30:07 -0700

    3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -8,8 +8,8 @@
 int factorial(int n)
 {
 if (n < 0)
-return -1;  /* If n is negative, return -1 to indicate an error. */
+return (-1);  /* If n is negative, return -1 to indicate an error. */
 if (n == 0)
-return 1;   /* Base case: factorial of 0 is 1. */
-return n * factorial(n - 1);  /* Recursive case. */
+return (1);   /* Base case: factorial of 0 is 1. */
+return (n * factorial(n - 1));  /* Recursive case. */
 }
